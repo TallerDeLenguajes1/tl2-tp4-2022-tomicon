6d46b4a067796c09a93b3551862c29d6b10b8d5d
Tomás <tomicon01@gmail.com>
2022-09-30 19:46:43 -0300

    Ejemplo de la clase

---
+++ Controllers/ejem1Controller.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Taller2_clase5.Models;
+
+namespace Taller2_clase5.Controllers
+{
+    public class ejem1Controller : Controller
+    {
+        private readonly ILogger<ejem1Controller> _logger;
+
+        public ejem1Controller(ILogger<ejem1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(Cliente miCliente)
+        {
+            
+            return View(miCliente);
+        }
+
+        public IActionResult ConsultaCliente()
+        {
+            var miCliente = new Cliente();
+            miCliente.name= "tomi";
+            miCliente.edad= 21;
+            return View(miCliente);
+        }
+
+        [HttpGet]
+        public IActionResult AltaCliente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AltaCliente(Cliente cliente)
+        {
+            return RedirectToAction("Index");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
